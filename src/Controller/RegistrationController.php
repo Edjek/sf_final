f7f4c635637d58a0e479c93618bfbe18dd1398b7
Edjek <edjek@hotmail.fr>
2022-01-24 19:38:27 +0100

    NavBar

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -141,4 +141,59 @@ class RegistrationController extends AbstractController
             'Le compte a été supprimé'
         );
     }
+
+    #[Route('/update/user/{id}', name: 'update_user_by_id')]
+    public function updateRegisterById(
+        $id,
+        Request $request,
+        UserRepository $userRepository,
+        UserPasswordHasherInterface $userPasswordHasher,
+        EntityManagerInterface $entityManager,
+    ): Response {
+        $user = $userRepository->find($id);
+        $form = $this->createForm(RegistrationFormType::class,  $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'notice',
+                'Le compte a été mis à jour'
+            );
+
+            return $this->redirectToRoute("admin_main");
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/delete/user/{id}', name: 'delete_user_by_id')]
+    public function deleteUserById($id, UserRepository $userRepository, EntityManagerInterface $entityManagerInterface)
+    {
+        $user = $userRepository->find($id);
+
+        $entityManagerInterface->remove($user);
+
+        $entityManagerInterface->flush();
+
+        $this->addFlash(
+            'notice',
+            'Le compte a été supprimé'
+        );
+
+        return $this->redirectToRoute("admin_main");
+    }
 }
--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ImageType extends AbstractType
@@ -15,7 +16,9 @@ class ImageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('src')
+            ->add('src', FileType::class, [
+                'mapped' => false
+            ])
             ->add('alt')
             ->add('title')
             ->add('product', EntityType::class, [
