527cc669852dd3627337e5a093d28401d236f756
Edjek <edjek@hotmail.fr>
2022-01-26 15:20:37 +0100

    Implement Globals gcategories and glicences

---
+++ src/Globals/Licences.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Globals;
+
+use App\Repository\LicenceRepository;
+
+class Licences
+{
+    private $licenceRepository;
+
+    public function __construct(LicenceRepository $licenceRepository)
+    {
+        $this->licenceRepository = $licenceRepository;
+    }
+
+    public function getAll()
+    {
+        $licences = $this->licenceRepository->findAll();
+
+        return $licences;
+    }
+}
